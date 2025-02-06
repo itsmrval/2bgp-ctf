a660b0267c87506891b08719910be9e10c422867
Valentin <43043885+itsmrval@users.noreply.github.com>
2025-02-06 10:57:16 +0100

    feat(backend) team type

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./src/config/database');
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,4 +17,11 @@ app.use('/points', require('./src/routes/points'));
 app.use('/teams', require('./src/routes/teams'));
 
 const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
