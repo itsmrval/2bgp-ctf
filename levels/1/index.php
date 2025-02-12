a2fd5ef82ada753671365ee4aeb01bb5c30bcc46
mukbeast78 <mazbersin@>
2025-02-12 15:30:55 +0100

    add mission 1

---
+++ levels/1/index.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau 2</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="#">
+                <img src="logo/starwars.png" alt="Star Wars Logo">
+            </a>
+        </div>
+    </nav>
+
+    <div class="content-wrapper">
+        <div class="news-section">
+            <div class="news-content">
+                <div class="news-image">
+                    <img src="logo/falcon_ship.png" alt="Star Wars News Image">
+                </div>
+
+                <div class="news-text">
+                    <h2>galactic new report : lost starship - coords needed !</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officia quo pariatur cumque voluptatibus
+                         necessitatibus exercitationem dignissimos, sit doloremque iure voluptatum explicabo veniam nostrum. Ea vero aliquam necessitatibus consequatur in!
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officia quo pariatur cumque voluptatibus
+                         necessitatibus exercitationem dignissimos, sit doloremque iure voluptatum explicabo veniam nostrum. Ea vero aliquam necessitatibus consequatur in!
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officia quo pariatur cumque voluptatibus
+                         necessitatibus exercitationem dignissimos, sit doloremque iure voluptatum explicabo veniam nostrum. Ea vero aliquam necessitatibus consequatur in!
+                    </p>
+                </div>
+            </div>
+            
+            <div class="coords-container">
+                <form action="" method="post">
+                    <div class="inputbox">
+                        <input type="coords" name="latitude" id="latitude" required>
+                        <label for="latitude">Latitude (degrés)</label>
+                    </div>
+                    <div class="inputbox">
+                        <input type="coords" name="longitude" id="longitude" required>
+                        <label for="longitude">Longitude (degrés)</label>
+                    </div>
+                    <?php
+                        if (isset($_POST['latitude']) && isset($_POST['longitude'])) {
+                            if ($_POST['latitude'] === '48' && $_POST['longitude'] === '2') {
+                                header("Location: sucess.php");
+                                $_SESSION['flag'] = "XXXXX";
+                            } else {
+                                echo '<div class="message error">Coordonnées invalides. Réessayer.</div>';
+                            }
+                        }
+                    ?>
+                    <div class="submit-btn">
+                        <button type="submit" class="btn">Envoyer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+
+    <footer class="footer">
+        <p>&copy; 2025 Star Wars - 2BGP-CTF. Tous droits réservés.</p>
+    </footer>
+</body>
+
+</html>
---
+++ levels/1/sucess.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['flag'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Level 2</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+    <nav class="nav">
+        <div class="logo">
+            <a href="#">
+                <img src="logo/starwars.png" alt="Star Wars Logo">
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="text-box">
+            <p>
+                Bien joué ! <br>
+                Tu as trouvé les coordonnées de la planète Naboo! Tu peux t'y rendre et continuer rapidement ta mission....<br>
+                <strong>FLAG={XXXX}</strong>
+
+            </p>
+        </div>
+    </div>
+</body>
+</html>
