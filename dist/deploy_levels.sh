0f6cf983eb719790545b714c5ae076161e59f260
Valentin <43043885+itsmrval@users.noreply.github.com>
2025-02-13 10:56:01 +0100

    feat(docker) deploy script

---
+++ dist/deploy_levels.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+source /scripts/config/backend.env
+hostname="http://backend:$PORT"
+token="$ADMIN_SYSTEM_TOKEN"
+
+# Function to send curl requests
+send_request() {
+    curl --location "$hostname/levels" \
+        --header 'Content-Type: application/x-www-form-urlencoded' \
+        --header "Authorization: Bearer $token" \
+        --data-urlencode "name=$1" \
+        --data-urlencode "hid=$2" \
+        --data-urlencode "description=$3" \
+        --data-urlencode "flag=$4" \
+        --data-urlencode "url=$5" \
+        --data-urlencode "points=$6"
+}
+
+# Check if levels exist by sending a GET request
+response=$(curl --location "$hostname/levels" \
+    --header "Authorization: Bearer $token")
+
+# If the response is an empty list, run the requests
+if [[ "$response" == "[]" ]]; then
+    # Request 1: Croiseur
+    send_request "Croiseur" 1 "Ton maitre te communique ta mission sur un holoprojecteur, déchiffre ce message et récupère le flag pour commencer ton aventure" "diabolic-atop-compress" "https://target1.2bgp-ctf.vpws.eu" 100
+
+    # Request 2: Endor
+    send_request "Endor" 2 "Vous pénétrez dans les recoins secrets de la dernière base des services secrets. Dans ce sanctuaire caché, une transmission clandestine a été interceptée, émanant du fils redouté de Valkorion, Arkann. Le message, énigmatique et porteur d'un défi contient une photographie qui, à première vue, semble n'être qu'une simple image du vaisseau perdu le Faucon Millenium.
+
+    Mais sous ces pixels se dissimule une énigme bien plus profonde. En laissant la Force guider votre intuition, vous vous devez de déchiffrez ces indices cachés." "untagged-disabled-arguable" "https://target2.2bgp-ctf.vpws.eu" 150
+
+    # Request 3: Naboo
+    send_request "Naboo" 3 "Après avoir retrouvé le vaisseau d’Arkann, une étrange inquiétude s’empare de vous : Arkann n’est nulle part en vue. Aurait-il délibérément abandonné son vaisseau pour vous mettre à l’épreuve ? Pourtant, ce dernier est hermétiquement verrouillé.
+
+    Mais la Force est avec vous. Concentrant votre énergie, vous percevez les subtils échos des enchantements protégeant l’entrée. D’un geste sûr, vous utilisez vos pouvoirs pour déverrouiller la porte, qui s’ouvre alors dans un léger murmure, révélant l’intérieur silencieux du cockpit.
+
+    Il est écrit sur l'écran : \"Vérifier votre identité\". Vous comprenez rapidement que ce secret ne peut être dévoilé que par un savant mélange de technologie et de maîtrise de la Force.
+
+    Agissant avec détermination, vous injectez un code permettant de prendre le contrôle du cockpit. Votre perspicacité vous guide vers un dossier dissimulé dans les entrailles du vaisseau." "reusable-bacon-cabbie" "https://target3.2bgp-ctf.vpws.eu" 150
+
+    # Request 4: Hoth
+    send_request "Hoth" 4 "Votre vaisseau traverse l'hyperespace pour atterrir sur la planète glaciale de Hoth, où, au cœur d’un paysage balayé par des vents hurlants, se dresse un ancien temple enveloppé de glace. Ce n’est pas un hasard si vous vous trouvez ici : cet édifice oublié est un sanctuaire renfermant des secrets capables de changer le cours de la guerre.
+
+    En fouillant les couloirs silencieux du temple, vous ressentez la présence de la Force guidant chacun de vos pas. Au centre d'une vaste salle, sur un piédestal de pierre, repose un holocron légendaire, un artefact sacré censé renfermer le savoir des anciens maîtres de la Force.
+
+    D’un geste assuré, vous vous attardez à déverrouiller ce précieux héritage. Avec la Force, vous trouverez le code protégeant l’holocron." "perky-snowfall-unease" "https://target4.2bgp-ctf.vpws.eu" 200
+
+    # Request 5: Kamino
+    send_request "Kamino" 5 "Vous atterrissez sur Kamino, la planète réputée pour ses prouesses en clonage. Dans les couloirs high-tech de la station, les Kaminoans, discrets et méthodiques, vous attendent. Ils vous informent qu’ils attendaient un certain Arkann, un nom qui résonne comme une ombre menaçante dans l'Empire. Vous comprenez alors qu'il vous faut vous faire passer pour ce fameux Arkann afin de ne susciter aucun soupçon. Ici, au cœur de ces laboratoires ultramodernes, se trame le sinistre dessein de l'Empereur Éternel : forger une armée de clones, basée sur l'ADN du chasseur de primes légendaire Jango Fett.
+
+    Le danger est imminent. Même sans cette armée, l'Empire Éternel est à deux doigts de plonger la galaxie dans un joug autoritaire impitoyable. Votre mission, aussi périlleuse qu'essentielle, consiste à pénétrer dans la salle secrète abritant l'ADN des clones, véritable noyau de l'opération. Là, un ordinateur central contrôle toutes les données génétiques. Vous devez vous connecter, mais le mot de passe du chasseur de primes n’est pas en votre possession. À vous de le retrouver." "storeroom-bonelike-dreamlike" "https://target5.2bgp-ctf.vpws.eu" 200
+
+    # Request 6: Tatooine
+    send_request "Tatooine" 6 "Après avoir atterri sur Tatooine, la chaleur implacable et les sables infinis de cette planète vous rappellent que le destin de la galaxie se joue parfois dans les lieux les plus inhospitaliers. Utilisant la Force, vous percevez des échos de présence dans l’obscurité des ruelles et des repaires cachés. Rapidement, vous localisez le célèbre chasseur de primes, Jango Fett.
+
+    Sans détour, vous le confrontez. Sous la pression de vos menaces et l’éclat déterminé dans vos yeux, Jango comprend qu’il ne peut plus se dérober à votre quête. Bien qu’il ne possède pas d’informations détaillées sur les plans de l’Empereur, il révèle une nouvelle capitale : une prime a été lancée pour libérer Vaylin, la fille de Valkorion, détenue dans une prison ultra-sécurisée.
+
+    Grâce à ses contacts, Jango a mis la main sur les plans de cette prison, situés sur la planète Nathema, dissimulés dans plusieurs images numériques. Chaque image représente une partie des installations, mais une faille technique vous offre l’accès à une information cruciale." "pacifism-neurosis-karma" "https://target6.2bgp-ctf.vpws.eu" 200
+
+    # Request 7: Nathema
+    send_request "Nathema" 7 "Arrivé sur la planète Nathema, vous vous faufilez dans les sombres corridors de la prison, votre objectif ultime étant de libérer Vaylin. Dans ce labyrinthe de couloirs glacials, une lourde porte retient la captive. Les gardes ont un réseau spécifique pour ouvrir la cellule (172.16.0.0/24)" "unsliced-unleveled-parrot" "https://target7.2bgp-ctf.vpws.eu" 250
+
+    # Request 8: Rodia
+    send_request "Rodia" 8 "Pendant votre périple vers Rodia, Vaylin vous confie un nouvel indice crucial. Dans un souffle empreint de nostalgie, elle se remémore les doux jours de son enfance sur Rodia, lorsqu’elle se réfugiait dans la bibliothèque de sa maison d’enfance. Dans cette bibliothèque reposait un ouvrage précieux : un livre dont les pages recelaient non seulement des secrets anciens, mais aussi le code qui avait scellé son destin. Ce livre, témoin silencieux de ses espoirs et de ses rêves, avait inspiré la création de son mot de passe, un sésame perdu au fil des années.
+
+    Votre mission s’annonce aussi périlleuse qu’essentielle : dénicher ce livre oublié et en extraire le mot de passe dissimulé entre ses lignes." "talon-scorpion-tightwad" "https://target8.2bgp-ctf.vpws.eu" 250
+
+    # Request 9: Flotte Eternelle
+    send_request "Flotte Eternelle" 9 "Vous vous tenez devant la flotte Éternelle, alors que votre vaisseau oscille dans l’ombre menaçante de cette armada interstellaire. Sur une planète lointaine, Vaylin a réussi à établir une connexion avec votre système, vous permettant d’écouter en direct sa conversation tendue avec son père redoutable." "cactus-hula-most" "https://target9.2bgp-ctf.vpws.eu" 250
+else
+    echo "Level already exists"
+fi
