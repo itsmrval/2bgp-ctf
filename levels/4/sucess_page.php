880e514353fba91ffa110d9e50e72417c18f5ef8
mukbeast78 <mazbersin@>
2025-02-13 13:52:51 +0100

    level 4 finish

--- levels/4/sucess_page.php
+++ levels/4/sucess_page.php
@@ -29,27 +29,46 @@ if (!isset($_SESSION['flag'])) {
         </div>
     </nav>
 
-    <div class="container">
-    <p class="title">Stockage d'email</p>
-
-    <!-- Faux email -->
-    <div class="email-container">
-        <div class="email-header">
-            <p><strong>De :</strong> ValkorionEmpereur@2bgp-ctf.com</p>
-            <p><strong>A :</strong> JangoFett1977@2bgp-ctf.com</p>
-            <p><strong>Sujet:</strong> Centre ADN </p>
-        </div>
-        <div class="email-body">
+    
+    <div class="container"> 
+    <!-- Début du nouveau conteneur -->
+    <div class="text-block">
+        <p class="title">Stockage d'email</p>
+        <p> 
+            Peu à peu, les couches de protection se dissolvent, et un mail s’affiche, dévoilant une sombre prophétie.   
+            <br><br>
+            Le texte révèle qu’un danger sans précédent se profile :
+        </p>
+        <!-- Faux email -->
+        <div class="email-container">
+            <div class="email-header">
+                <p><strong>De :</strong> ValkorionEmpereur@2bgp-ctf.com</p>
+                <p><strong>A :</strong> JangoFett1977@2bgp-ctf.com</p>
+                <p><strong>Sujet:</strong> Centre ADN </p>
+            </div>
+            <div class="email-body">
             <p>Jango,</p>
-            <p>S'il te plait, va sur la planete Kamino et créer des clones, c'est urgent...  Important : souviens toi de mon email.</p>
-            <p>A bientot, GL</p>
+            <p>Pour remporter cette guerre, une armée de clones m'est indispensable. Tu dois être l’ADN originel, le modèle parfait à partir duquel naîtront mes soldats d’élite. Pars immédiatement pour Kamino et exécute ta mission sans la moindre hésitation. Sache que ton engagement ne passera pas inaperçu : tes compétences seront récompensées grassement et ta loyauté te vaudra une compensation à la hauteur de l’enjeu.
+            </p>
+            <p>N’oublie pas ton adresse mail secret, c’est la clé pour sceller ton ADN sur Kamino et garantir le succès de notre entreprise.</p>
             <p>- Valkorion</p>
             <p><strong>PS:</strong> FLAG={perky-snowfall-unease}</p>
-        </div>
+            </div>
+        </div>  
+        <br>    
+        <p> 
+        Vous devez retourner à votre vaisseau et vous rendre sur Kamino pour détruire l'ADN de Jango Fett.  
+        </p>
+
     </div>
 </div>
+
 </body>
 <footer class="footer">
     <p>&copy; 2025 Star Wars - 2BGP-CTF. Tous droits réservés.</p>
 </footer>
 </html>
+
+
+
+
